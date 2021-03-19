698748c2774d30b97d1adfd7311827247433fde7
Volodymyr <volodymyr.dovbenko@gmail.com>
2021-03-19 09:56:46 +0200

    Added global preloader.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Slideshow App';
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
+  ) {
+  }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.document.getElementById('loader'), 'display', 'none');
+  }
 }
